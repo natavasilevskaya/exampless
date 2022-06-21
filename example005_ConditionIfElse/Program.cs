d0a6a61759ad2483a63e0718c265faf4469801af
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-06-21 16:48:44 +0300

    Condition examples added

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "ната")
+{
+    Console.WriteLine("Ура, это же Ната!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
