8a470b6eecc1727ff7178da20f47ce6228a8b60a
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-06-21 16:05:24 +0300

    Random numbers divide added

--- example004_Dif/Program.cs
+++ example004_Dif/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 310;
-double  numberB = 305;
-double  result = numberA / numberB;
+﻿int numberA = new Random(). Next (22, 94);
+Console.WriteLine(numberA);
+int numberB = new Random(). Next (22, 94);
+Console.WriteLine(numberB);
+int result = numberA / numberB;
 Console.WriteLine(result);
