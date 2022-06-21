af59b28998b52b6400c3ac5922226c14f2411a02
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-06-21 17:24:25 +0300

    example006_Base added

---
+++ example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 21;
+int c = 123;
+int d = 66;
+int e = 99;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
